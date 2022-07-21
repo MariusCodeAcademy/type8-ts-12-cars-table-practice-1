d5c04a536779b72dc731ae7f6308e52709c118b9
Marius <marius@example.com>
2022-07-21 12:03:56 +0300

    joinCar

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,7 +1,33 @@
+import Car from '../types/car';
+import Model from '../types/model';
+import Brand from '../types/brand';
+import CarJoined from '../types/car-joined';
 // type CarsCollectionProps = {
 
 // };
 
-class CarsCollection {}
+class CarsCollection {
+  constructor(private cars: Car[], private models: Model[], private brands: Brand[]) {}
+
+  private joinCar(carId: string): CarJoined | undefined {
+    // id: '2ED35B36-4EA2-83DF-2BCD-AB32E95DBC62'
+    const foundCar: Car | undefined = this.cars.find((c: Car) => c.id === carId);
+    if (!foundCar) return;
+    const foundModel: Model | undefined = this.models.find((m: Model) => m.id === foundCar.modelId);
+    if (!foundModel) return;
+    const foundBrand: Brand | undefined = this.brands.find(
+      (b: Brand) => b.id === foundModel.brandId
+    );
+    if (!foundBrand) return;
+    // sujungti i viena
+    const { modelId, ...restCar } = foundCar;
+    const joinedCar: CarJoined = {
+      ...restCar,
+      brand: foundBrand.title,
+      model: foundModel.title,
+    };
+    return joinedCar;
+  }
+}
 
 export default CarsCollection;
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import App from './components/app';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import CarsCollection from './helpers/cars-collection';
 
 const app = new App('#root');
 app.initialize();
