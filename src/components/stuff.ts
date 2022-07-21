f2bd04c53c83c5a161218cd7630af1a5b1436aef
Marius <marius@example.com>
2022-07-21 12:40:55 +0300

    aha

--- src/components/stuff.ts
+++ src/components/stuff.ts
@@ -1,4 +1,4 @@
-function stuff(): void {
+function stuff(b): void {
   console.log('stuff');
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import App from './components/app';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import CarsCollection from './helpers/cars-collection';
+// import CarsCollection from './helpers/cars-collection';
 
 const app = new App('#root');
 app.initialize();
